ef140994b57da94b754978aa92ef86e0e5020d4c
Michal <michal@blesu.com>
2021-10-05 17:01:07 +0200

    explicit cors

--- public/connectors/socketio.js
+++ public/connectors/socketio.js
@@ -4,4 +4,9 @@ exports.io = exports.httpServer = void 0;
 var http_1 = require("http");
 var socket_io_1 = require("socket.io");
 exports.httpServer = http_1.createServer();
-exports.io = new socket_io_1.Server(exports.httpServer, {});
+exports.io = new socket_io_1.Server(exports.httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
--- src/connectors/socketio.ts
+++ src/connectors/socketio.ts
@@ -2,5 +2,10 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 export const httpServer = createServer();
-export const io = new Server(httpServer, {});
+export const io = new Server(httpServer, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"]
+	  }
+});
 
