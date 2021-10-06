2d59e4a4e68fd6ec578adc3b07526b7682bf8c98
Michal <michal@blesu.com>
2021-10-06 15:59:30 +0200

    file struct refactor

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 require("dotenv").config();
 import { io, httpServer } from "./connectors/socketio"
+import redis from "./connectors/redis"
 import { initObserver } from "./listeners/initObserver"
 import { Socket } from "socket.io";
+import { EventModel } from "./models/event"
 
 if (!process.env.PORT ||
 	!process.env.REDIS ||
@@ -22,5 +24,24 @@ io.on("connection", (socket: Socket) => {
 		socket.leave(room)
 	})
 });
+
+
+redis.on("message", async (channel, message) => {
+	console.log(`Received ${message} from ${channel}`);
+	try {
+		const event = JSON.parse(message) as EventModel
+		if ("broadcast" in event) {
+			io.emit(event.name, event.payload)
+			return	
+		}
+		await Promise.all(event.rooms.map(room => {
+			io.to(room).emit(event.name, event.payload)
+		}))
+	} catch (error) {
+		console.log(error)
+		return
+	}
+});
+
 
 httpServer.listen(process.env.PORT, () => console.log("success"))
---
+++ src/connectors/redis.ts
@@ -0,0 +1,14 @@
+import Redis from "ioredis"
+
+const redis = new Redis(parseInt(process.env.REDISPORT!), process.env.REDIS);
+
+redis.subscribe("event", (error, count) => {
+	if (error) {
+		console.error("Failed to subscribe");
+		console.log(error)
+	} else {
+		console.log("Success")
+	}
+});
+
+export default redis
